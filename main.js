a9003ff575b3ddb89bd0115bc1f9b081218723d6
kirtan-shah <kirtanushah@gmail.com>
2017-03-02 21:43:04 -0800

    Update Stuff

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
   $("#bar").css({
     "width" : window.innerWidth + "px",
-    "height": (window.innerHeight * 0.08) + "px"
+    "height" : (window.innerHeight * 0.17) + "px"
+  });
+  $("#leftmenu").css({
+     "top": $("#bar").height() + "px",
+     "width" : (window.innerWidth * 0.15) + "px",
+     "height" : (window.innerHeight - $("#bar").height()) + "px"
   });
 });
