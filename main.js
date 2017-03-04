1f569505743ed02b9fd2e03dbfd3c6468556bda6
kirtan-shah <kirtanushah@gmail.com>
2017-03-04 00:30:25 -0800

    Add button fade

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
 $(document).ready(function() {
-    /*
-    $("#bar").css({
-        "width" : window.innerWidth + "px",
-        "height" : (window.innerHeight * 0.12) + "px"
-    });
-    */
+
     var header = createHeader({
         width: window.innerWidth,
         height: window.innerHeight * 0.08,
@@ -12,4 +7,5 @@ $(document).ready(function() {
         items: ["About", "Projects", "Programs", "Blog"]
     });
     $(document.body).append(header);
+
 });
